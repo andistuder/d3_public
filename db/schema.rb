2d276a516d846ffe1d81658d63b8338f77c8762c
andistuder <andi@cenatus.org>
2012-02-16 15:11:19 +0000

    reduced border size to 1px across..

--- db/schema.rb
+++ db/schema.rb
@@ -108,7 +108,7 @@ ActiveRecord::Schema.define(:version => 20120216125420) do
     t.string   "username"
     t.integer  "item"
     t.string   "table"
-    t.integer  "month"
+    t.integer  "month",      :limit => 2
     t.integer  "year",       :limit => 8
     t.datetime "created_at"
     t.datetime "updated_at"
