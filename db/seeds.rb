16aa93f0abe28a4b47e1966c71f52aeec62fa96a
andistuder <andi@cenatus.org>
2012-02-21 15:52:50 +0000

    seeded partner categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+
+
+PARTNERCATS = {
+  "users" => "Users",
+  "rental_partners" => "Rental Partners",
+  "distributors" => "Distributors",
+  "previs_studios" => "Previs Studios"
+}
+
+PARTNERCATS.each do |slug, name|
+  attributes = {:name => name, :slug => slug}
+  PartnerCategory.create(attributes) unless PartnerCategory.exists?(attributes)
+end
