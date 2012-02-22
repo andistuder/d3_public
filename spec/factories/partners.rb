766c80067a2b4cb20413c2c947a340f3b1a19693
andistuder <andi@cenatus.org>
2012-02-22 12:29:39 +0000

    added project category show view

--- spec/factories/partners.rb
+++ spec/factories/partners.rb
@@ -5,6 +5,13 @@ FactoryGirl.define do
     slug "pa-slug"
     content Faker::Lorem.paragraph
     created_at Time.now
+    #association :partner_category
+    town "London"
+    postcode "E1 111"
+    country "United Kingdom"
+    email "dummy@user.com"
+    website "http://user.com"
+    phone "012 345 6799"
   end
 
   factory :partner_category, :class => PartnerCategory do
@@ -12,6 +19,7 @@ FactoryGirl.define do
     slug "pac-slug"
     content Faker::Lorem.paragraph
     created_at Time.now
+    #association :partners
   end
 
 end
