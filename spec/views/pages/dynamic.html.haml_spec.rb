e420400e2437a4858d51db5afd7c178db40be357
msp <info@radialsolutions.co.uk>
2012-02-23 15:24:23 +0000

    Dynamic pages: fix bug if no layout exists

--- spec/views/pages/dynamic.html.haml_spec.rb
+++ spec/views/pages/dynamic.html.haml_spec.rb
@@ -12,4 +12,18 @@ describe "pages/dynamic.html.haml" do
 
   end
 
+  it "renders with non existent partial, using content areas" do
+
+    @page = FactoryGirl.build(:tutorial_page, :name => "tutorial", :slug => "xx-this-partial-doesnt-exist")
+    @page_content_area = FactoryGirl.build(:user_guide, :name => "user guide", :content => "user guide content area")
+    @page.should_receive(:content_areas).and_return([@page_content_area])
+    assign(:page, @page)
+
+    render
+    rendered.should have_content(@page.name)
+    rendered.should have_content(@page_content_area.name)
+    rendered.should have_content(@page_content_area.content)
+
+  end
+
 end
