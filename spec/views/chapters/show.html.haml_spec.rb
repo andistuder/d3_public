7edfc1621505481fd83010101f712db8453aa604
msp <info@radialsolutions.co.uk>
2012-02-26 20:00:29 +0000

    Workaround this encoding-in-tests problem by forcing content rather thn using Faker stuff that includes quotes etc

--- spec/views/chapters/show.html.haml_spec.rb
+++ spec/views/chapters/show.html.haml_spec.rb
@@ -3,8 +3,8 @@ require 'spec_helper'
 describe "chapters/show.html.haml" do
   it "renders" do
 
-    @chapter = FactoryGirl.build :chapter
-    @chapter_content_area = FactoryGirl.build :chapter_content_area
+    @chapter = FactoryGirl.build :chapter, :content => "Some content without any funky characters that will make the test pass"
+    @chapter_content_area = FactoryGirl.build :chapter_content_area, :content => "Some OTHER content without any funky characters that will make the test pass"
 
     @chapter.should_receive(:content_areas).and_return([@chapter_content_area])
 
@@ -16,7 +16,7 @@ describe "chapters/show.html.haml" do
     rendered.should have_content(@chapter.name)
     rendered.should_not have_content(@chapter.summary)
     #TODO MSP this encoding stuff still isn't right
-    rendered.should have_content(textilize(@chapter.content).html_safe)
+    rendered.should have_content(@chapter.content)
 
     rendered.should have_content(@chapter_content_area.title)
     rendered.should have_content(@chapter_content_area.content)
