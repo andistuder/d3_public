df28da00cf42257cd79f2b6c0d240f7deeff4a26
andistuder <andi@cenatus.org>
2012-02-28 17:55:49 +0000

    added styling for form

--- app/controllers/enquiries_controller.rb
+++ app/controllers/enquiries_controller.rb
@@ -1,11 +1,12 @@
 class EnquiriesController < ApplicationController
 
   def send_enquiry
-    #TODO validate send or raise error, redirect to back?
-    FormMailer.enquiry_email(params).deliver
-    redirect_to('/', :notice => "Enquiry was successfully sent.")
-
+    #TODO validate send or raise error, below does not pick up delivery error. redirect to back?
+    if FormMailer.enquiry_email(params).deliver
+      redirect_to('/', :notice => "Enquiry was successfully sent.")
+    else
+      redirect_to(:back, :notice => "Something went wrong. Please try again or contact us on info@d3technologies.com")
+    end
     #redirect_to honey_path(@honey), :notice => "Thank you for your purchase order! The seller has been notified and will be in touch shortly." and return
-
   end
 end
--- app/mailers/form_mailer.rb
+++ app/mailers/form_mailer.rb
@@ -4,10 +4,10 @@ class FormMailer < ActionMailer::Base
   def enquiry_email(form_content)
     @form_content = form_content
     #@url  = "http://example.com/login"
-    puts "ANDI#{@form_content[:email]}"
+    #puts "ANDI#{@form_content[:email]}"
     #TODO change to correct email setting
     #mail(:to => "d3@cenatus.org", :subject => "d3 web enquiry")
-    mail(:to => "andi@cenatus.org", :subject => "d3 web enquiry")
+    mail(:to => "andi@cenatus.org", :subject => "d3 web enquiry test")
   end
 
 end
