511586ef834bbef8295953b361119b0c0a9a7179
andistuder <andi@cenatus.org>
2012-02-29 10:02:23 +0000

    mailer configuration update

--- app/controllers/enquiries_controller.rb
+++ app/controllers/enquiries_controller.rb
@@ -1,12 +1,8 @@
 class EnquiriesController < ApplicationController
 
   def send_enquiry
-    #TODO validate send or raise error, below does not pick up delivery error. redirect to back?
-    if FormMailer.enquiry_email(params).deliver
-      redirect_to('/', :notice => "Enquiry was successfully sent.")
-    else
-      redirect_to(:back, :notice => "Something went wrong. Please try again or contact us on info@d3technologies.com")
-    end
-    #redirect_to honey_path(@honey), :notice => "Thank you for your purchase order! The seller has been notified and will be in touch shortly." and return
+    FormMailer.enquiry_email(params).deliver rescue redirect_to(:back, :alert => "Something went wrong. Please try again or contact us on info@d3technologies.com")
+    redirect_to('/', :notice => "Enquiry was successfully sent.") and return
+
   end
 end
