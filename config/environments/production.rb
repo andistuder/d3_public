b25fc82563e6d2c31630289e16e79b09868ce2f5
andistuder <andi@cenatus.org>
2012-03-01 08:49:51 +0000

    cukes and mailer settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,6 +47,7 @@ D3::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   #config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
 
   # Enable threaded mode
--- features/support/paths.rb
+++ features/support/paths.rb
@@ -29,7 +29,7 @@ module NavigationHelpers
       projects_path
 
     when /the user guide/
-      "/support/knowledge-base/user-guide" #TODO sort paths here
+      "/support/user-guide" #TODO sort paths here
 
     when /the case studies page/
       case_studies_path
