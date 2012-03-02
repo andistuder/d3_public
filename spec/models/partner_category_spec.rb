b474f7ba6a6d4b644adc3fd47d5d7674e3af9bd0
andistuder <andi@cenatus.org>
2012-03-02 14:32:34 +0000

    CUKES

--- spec/models/partner_category_spec.rb
+++ spec/models/partner_category_spec.rb
@@ -12,19 +12,19 @@ describe PartnerCategory do
     ProjectCategory.new.should be_an_instance_of(ProjectCategory)
   end
 
-  describe "database finders" do
 
-    describe "find_in_oder" do
-      before :each do
-        @partner_cat1 = FactoryGirl.create(:partner_category, :sort_order => 1)
-        @partner_cat2 = FactoryGirl.create(:partner_category, :sort_order => 2)
-      end
 
-      it "should load all records, by sort order asc" do
-        PartnerCategory.find_in_order.should eq([@partner_cat1, @partner_cat2])
-      end
+  describe "find_in_oder" do
+    before :each do
+      @partner_cat1 = FactoryGirl.create(:partner_category, :sort_order => 1)
+      @partner_cat2 = FactoryGirl.create(:partner_category, :sort_order => 2)
+    end
+
+    it "should load all records, by sort order asc" do
+      PartnerCategory.find_in_order.should eq([@partner_cat1, @partner_cat2])
     end
   end
+
 
 
 end
