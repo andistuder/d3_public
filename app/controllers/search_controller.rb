4fa5316d51f7a89708a6298589fdb43ddb1512f6
andistuder <andi@cenatus.org>
2012-03-09 18:05:37 +0000

    update search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,14 +1,15 @@
 class SearchController < ApplicationController
   def search
     @projects = Project.find_with_index(params[:query],{:limit => 20})
-    @news = NewsItem.find_with_index(params[:query],{:limit => 10})
+    @news = NewsItem.with_query(params[:query]).order('created_at DESC').find(:all, :limit => 10)
     @features = Feature.find_with_index(params[:query],{:limit => 30})
     @case_studies = CaseStudy.find_with_index(params[:query],{:limit => 10})
     @partners = Partner.find_with_index(params[:query],{:limit => 10})
-    @downloads = Download.find_with_index(params[:query],{:limit => 20})
+    @downloads = Download.with_query(params[:query]).order('created_at DESC').find(:all, :limit => 10)
     @faqs = Faq.find_with_index(params[:query],{:limit => 20})
     @chapters = ChapterContentArea.find_with_index(params[:query],{:limit => 20})
-    #@tutorials = ContentArea.where(:page.name => 'Tutorials').find_with_index(params[:query],{:limit => 10})
+    page_id = Page.find_all_name('Tutorials').id
+    @tutorials = ContentArea.with_query(params[:query]).where(:page_id => page_id).limit(10)
   end
 
 end
