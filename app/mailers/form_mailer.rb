fc3d057f8738168968742c29626b14927dc57d49
andistuder <andi@cenatus.org>
2012-03-16 15:59:59 +0000

    mailer enabled

--- app/mailers/form_mailer.rb
+++ app/mailers/form_mailer.rb
@@ -1,21 +1,13 @@
 class FormMailer < ActionMailer::Base
-  #TODOenter custom email?
   default :from => "webmailer@cenatus.org"
 
   def enquiry_email(form_content)
     @form_content = form_content
-    #TODO enter right email
-    #mail(:to => "enquiries@d3technologies.com", :subject => "d3 web enquiry")
-    mail(:to => "andi@cenatus.org", :subject => "d3 web enquiry")
+    mail(:to => "enquiries@d3technologies.com", :subject => "d3 web enquiry")
   end
 
   def mailer_email(form_content)
     @form_content = form_content
-    #TODO enter right email
-    #mail(:to => "mailinglist@d3technologies.com", :subject => "mailing list subscription")
-    mail(:to => "andi@cenatus.org", :subject => "mailing list subscription")
+    mail(:to => "mailinglist@d3technologies.com", :subject => "mailing list subscription")
   end
-
-
-
 end
