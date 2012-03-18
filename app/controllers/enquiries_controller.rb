1bfe03067fec80849b6141b559fb61c3395c5e98
andistuder <andi@cenatus.org>
2012-03-18 22:02:57 +0000

    fix enquiries form

--- app/controllers/enquiries_controller.rb
+++ app/controllers/enquiries_controller.rb
@@ -1,8 +1,8 @@
 class EnquiriesController < ApplicationController
 
   def send_enquiry
-    FormMailer.enquiry_email(params).deliver rescue redirect_to('/enquiry-form', :alert => "Something went wrong. Please try again or contact us on info@d3technologies.com")
-    redirect_to('/enquiry-form', :notice => "Thank you for you interest in d3. One of our team members will get back to shortly.") and return
+    FormMailer.enquiry_email(params).deliver rescue redirect_to('/company/enquiry-form', :alert => "Something went wrong. Please try again or contact us on info@d3technologies.com") and return
+    redirect_to('/company/enquiry-form', :notice => "Thank you for you interest in d3. One of our team members will get back to shortly.") and return
   end
 
   def send_subscription
