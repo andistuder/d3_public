85c481221ca0cd41d4e1aecda5176945537a12ea
andistuder <andi@cenatus.org>
2012-05-10 17:50:34 +0100

    udated mailer settings

--- app/mailers/form_mailer.rb
+++ app/mailers/form_mailer.rb
@@ -1,13 +1,13 @@
 class FormMailer < ActionMailer::Base
-  # default :from => "webmailer@cenatus.org"
+  default :from => "webmailer@cenatus.org"
 
   def enquiry_email(form_content)
     @form_content = form_content
-    mail(:to => "enquiries@d3technologies.com", :from => @form_content[:email], :subject => "d3 web enquiry from #{@form_content[:first_name]} #{@form_content[:last_name]}")
+    mail(:to => "enquiries@d3technologies.com", :reply_to =>  @form_content[:email], :subject => "d3 web enquiry from #{@form_content[:first_name]} #{@form_content[:last_name]}")
   end
 
   def mailer_email(form_content)
     @form_content = form_content
-    mail(:to => "mailinglist@d3technologies.com", :from => "webmailer@cenatus.org", :subject => "mailing list subscription")
+    mail(:to => "mailinglist@d3technologies.com", :subject => "mailing list subscription")
   end
 end
